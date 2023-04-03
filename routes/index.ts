48333c631eea4e716a19fc1a58bfc90d65672be8
Ari <ari@jaytap.com>
2023-04-03 19:37:17 -0400

    routes file

--- routes/index.ts
+++ routes/index.ts
@@ -1,63 +1,24 @@
 import express, { Request, Response } from 'express';
-import multer from 'multer';
-import path from 'path'
+import { PrismaClient } from '@prisma/client';
 
+const server = express();
+const prisma = new PrismaClient();
 
-import whisper from 'whisper-node'
+// route to test that prisma saves
+server.post('/prisma-create', async (req: Request, res: Response) => {
+  const { ts } = req.body;
 
+  // 1. read json from request body
+  // 2. save json to prisma
+  // 3. query prisma for saved json
+  // 4. send prisma query response back to client
 
-const app = express();
-export default app;
+  // Pass 'user' object into query
+  const newClip = await prisma.marker.create({ data: { ts } });
 
+  res.json(newClip);
 
-//Configuration for Multer storatge
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const destination = path.join(__dirname, '../', 'temp');
-    console.log("this is the destination that multer is saving to (done with path.join)", destination);
-    cb(null, destination);
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  },
-});
-
-// Multer Filter
-const multerFilter = (req, file, cb) => {
-  if (file.mimetype.split("/")[1] === "wave" || file.mimetype.split("/")[1] === "wav") {
-    cb(null, true);
-  } else {
-    cb(new Error("Not a WAV file File!!"), false);
-  }
-};
-
-const upload = multer({
-  storage: multerStorage,
-  fileFilter: multerFilter,
 });
 
 
-
-
-
-// use multer or something idk we got to figure this out
-app.post('/submit', upload.single('audio'), async (req: Request, res: Response, next: Function) => {
-  try {
-
-    // todo: the path.join is seemingly unnecessary
-    // might be needed for passing to a node module (whisper-json)
-    // const filePath = path.join(req.file.path);
-    const filePath = req.file.path;
-    
-    console.log("file path", filePath);
-
-    const transcript = await whisper(filePath);
-
-    res.send(transcript)
-
-  } catch (error) {
-    console.log("express ground", error);
-    throw error;
-  }
-});
-
+export default server;
