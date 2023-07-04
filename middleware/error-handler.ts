b4e06d1a72a069ebbc51a822915690a8b905938a
Ari <ari@mangotap.com>
2023-07-04 00:21:07 -0400

    fixed error handler.

--- middleware/error-handler.ts
+++ middleware/error-handler.ts
@@ -1,5 +1,16 @@
-import {Request, Response, NextFunction} from 'express';
-export default (req: Request, res: Response, next: NextFunction) => {
-  res.status(404);
-  next(new Error('Not Found'));
+import { Request, Response, NextFunction } from 'express';
+export default (err: Error, req: Request, res: Response, next: NextFunction) => {
+  // Generic catch-all for errors.
+  if (err) {
+    console.log("generic error caught", err);
+
+    // if status less than 400 use generic 500 code
+    let status = res.statusCode
+    if (status < 400) {
+      status = 500;
+    }
+
+    res.status(status)
+    res.json({ status, message: err.message })
+  }
 }
--- middleware/index.ts
+++ middleware/index.ts
@@ -1,3 +1,2 @@
 export {default as logger} from './logger';
 export {default as errorHandler} from './error-handler';
-export {default as notFoundError} from './error404';
